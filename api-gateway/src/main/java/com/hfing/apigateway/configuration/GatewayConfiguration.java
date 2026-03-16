9bb6fd5ca7e117b3087457047e7a1f2faa14d0e5
Nguyễn Thái Hoàng <148040890+HFing@users.noreply.github.com>
2026-03-16 23:05:48 +0700

    Service Discovery with Eureka

--- api-gateway/src/main/java/com/hfing/apigateway/configuration/GatewayConfiguration.java
+++ api-gateway/src/main/java/com/hfing/apigateway/configuration/GatewayConfiguration.java
@@ -12,7 +12,7 @@ public class GatewayConfiguration {
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service", r -> r.path("/api/v1/users/**", "/api/v1/auth/**")
-                        .uri("http://localhost:8080"))
+                        .uri("lb://USER-SERVICE"))
                 .build();
     }
 }
---
+++ service-discovery/src/test/java/com/hfing/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfing.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
